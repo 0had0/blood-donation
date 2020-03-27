b9c2a8920c1145d842a82ebe0a984cee96824be2
hadi houssainy <hadihoussainy75@gmail.com>
2020-03-27 18:46:04 +0200

    Updates

---
+++ precache-manifest.909124eeb1e908262050740f38a72aba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e16fc164aba257477cceb52bee5ed9",
+    "url": "/blood-donation/index.html"
+  },
+  {
+    "revision": "da1e38d23bb8aa9bbcb2",
+    "url": "/blood-donation/static/css/main.19f92c12.chunk.css"
+  },
+  {
+    "revision": "014cd6bd5e6cefd0939e",
+    "url": "/blood-donation/static/js/2.7471a04f.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/blood-donation/static/js/2.7471a04f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da1e38d23bb8aa9bbcb2",
+    "url": "/blood-donation/static/js/main.6932b913.chunk.js"
+  },
+  {
+    "revision": "56ac26b7f8480e35b087",
+    "url": "/blood-donation/static/js/runtime-main.8443409c.js"
+  },
+  {
+    "revision": "64429dc1e728b92635990ae79e80ba04",
+    "url": "/blood-donation/static/media/bkg.64429dc1.svg"
+  },
+  {
+    "revision": "0793c3db2313bf03019317f470323e5f",
+    "url": "/blood-donation/static/media/blood.0793c3db.svg"
+  }
+]);
